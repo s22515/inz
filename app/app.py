deabbf741aea5f9d8f15a4bd676546f3c7b52f37
Patryk Polnik <s22515@pjwstk.edu.pl>
2024-05-08 18:13:19 +0200

    app code and models

---
+++ app/app.py
@@ -0,0 +1,108 @@
+import streamlit as st
+import pickle
+import numpy as np
+from app_functions import *
+
+
+gender_list = ['M','F']
+areas_list = ['Southwest', 'Central', 'N Hollywood', 'Mission', 'Devonshire',
+       'Northeast', 'Harbor', 'Van Nuys', 'West Valley', 'West LA',
+       'Wilshire', 'Pacific', 'Rampart', '77th Street', 'Hollenbeck',
+       'Southeast', 'Hollywood', 'Newton', 'Foothill', 'Olympic',
+       'Topanga']
+descent_list = ['B', 'H', 'W', 'A']
+premis_list = ['Residential building', 'Street', 'Other inside', 'Store',
+       'Restaurant/Bar', 'Parking', 'Public transport facilities',
+       'Medical facility', 'Vehicle', 'Other outside', 'Park', 'School']
+
+st.title("Identification of crime exposure using machine learning models.")
+
+
+geo_coordinates_dict = {
+   'Southwest': [34.0376, 33.9950, -118.2744, -118.3805],
+   'Central': [34.0761, 34.0296, -118.2259, -118.274], 
+   'N Hollywood': [34.2199, 34.117, -118.318, -118.4263], 
+   'Mission': [34.3343, 34.0144, -118.2653, -118.5047], 
+   'Devonshire': [34.333, 34.22, -118.4555, -118.6324], 
+   'Northeast': [34.1586, 34.0687, -118.1677, -118.3282], 
+   'Harbor': [34.0075, 33.7061, -118.2216, -118.3289], 
+   'Van Nuys': [34.2149, 34.1264, -118.4053, -118.4738], 
+   'West Valley': [34.2214, 33.9465, -118.2781, -118.5665], 
+   'West LA': [34.1324, 34.0266, -118.3761, -118.5713], 
+   'Wilshire': [34.089, 34.0319, -118.3174, -118.3897], 
+   'Pacific': [34.0317, 33.9165, -118.3602, -118.482], 
+   'Rampart': [34.0925, 34.0377, -118.2478, -118.3091], 
+   '77th Street': [34.0038, 33.9382, -118.2564, -118.3586], 
+   'Hollenbeck': [34.1115, 34.0128, -118.1554, -118.23], 
+   'Southeast': [33.9602, 33.8729, -118.2279, -118.2918], 
+   'Hollywood': [34.1346, 34.0575, -118.3004, -118.3955], 
+   'Newton': [34.0407, 33.9747, -118.2235, -118.2809], 
+   'Foothill': [34.294, 34.2065, -118.2658, -118.4415], 
+   'Olympic': [34.0836, 34.0371, -118.2831, -118.3239], 
+   'Topanga': [34.2427, 34.1356, -118.5622, -118.6676]
+   }
+
+form = st.empty()
+
+if 'page' not in st.session_state:
+    st.session_state.page = 'Form'
+
+if st.session_state.page == 'Form':
+   with form.container():
+      gender = st.selectbox(
+         "Choose your gender",
+         gender_list,
+         index=0,
+         placeholder="Indeterminate",
+      )
+
+      descent = st.selectbox(
+         "Choose descent",
+         descent_list,
+         index=0,
+         placeholder="Indeterminate",
+      )
+
+      age = st.number_input("Age", min_value=0, max_value=120, value="min")
+
+      time = st.time_input(
+         "Choose time", value=None, step=60)
+
+      date = st.date_input("Choose date", value="default_value_today", format="YYYY/MM/DD")
+
+      area = st.selectbox(
+         "Choose area name (based on LAPD area stations)",
+         areas_list,
+         index=0,
+         placeholder="Indeterminate",
+      )
+
+      geo_LAT = st.number_input(
+         f"type latitiude beetwen {geo_coordinates_dict[area][0]} and {geo_coordinates_dict[area][1]}",
+         max_value=geo_coordinates_dict[area][0],
+         min_value=geo_coordinates_dict[area][1],
+         step=0.001,
+         format="%.4f")
+
+      geo_LON = st.number_input(
+         f"type latitiude beetwen {geo_coordinates_dict[area][2]} and {geo_coordinates_dict[area][3]}",
+         max_value=geo_coordinates_dict[area][2],
+         min_value=geo_coordinates_dict[area][3],
+         step=0.001,
+         format="%.4f")
+
+      premis = st.selectbox(
+         "Choose short description of area",
+         premis_list,
+         index=0,
+         placeholder="Indeterminate",
+      )
+
+      if st.button('Predict'):
+         pred = get_prediction(area, gender, descent, premis, time, age, geo_LAT, geo_LON, date)
+         st.session_state.page = 'Prediction'
+
+
+if st.session_state.page == 'Prediction':
+   form.empty()
+   st.markdown(f"<div align='center', style='font-size:20px'>{pred}</div>", unsafe_allow_html=True)
---
+++ app/app_functions.py
@@ -0,0 +1,78 @@
+import pickle
+import numpy as np
+from sklearn.preprocessing import LabelEncoder, OneHotEncoder,  MinMaxScaler
+from xgboost import XGBClassifier
+import datetime
+
+
+def load_label_encoder() -> LabelEncoder:
+    with open('../models/label_encoder.pkl','rb') as f:
+        label_encoder = pickle.load(f)
+    return label_encoder
+
+def load_one_hot_encoder() -> OneHotEncoder:
+    with open('../models/one_hot_encoder.pkl','rb') as f:
+        one_hot_encoder = pickle.load(f)
+    return one_hot_encoder
+
+def load_min_max_scaler() -> MinMaxScaler:
+    with open('../models/min_max_scaler.pkl','rb') as f:
+        min_max_scaler = pickle.load(f)
+    return min_max_scaler
+
+def load_xgb_model() -> XGBClassifier:
+    with open('../models/xgb.pkl','rb') as f:
+        xgb_model = pickle.load(f)
+    return xgb_model
+
+def convert_time(time: datetime.time) -> int:
+    #converting time from ex. 12:00 to int 1200
+    time_converted = str(time.hour) + str(time.minute)
+    return int(time_converted)
+
+def get_prediction(
+    area: str, 
+    gender: str, 
+    descent: str, 
+    premis: str, 
+    time: datetime.time, 
+    age: int, 
+    geo_LAT: float,
+    geo_LON: float,
+    date: datetime.date) -> str:
+    
+    label_encoder = load_label_encoder()
+    one_hot_encoder = load_one_hot_encoder()
+    min_max_scaler = load_min_max_scaler() 
+    xgb_model = load_xgb_model()
+
+    categorical_to_encode = [area, gender, descent, premis]
+    categorical_encoded = one_hot_encoder.transform([categorical_to_encode])
+
+    time = convert_time(time)
+
+    numeric_to_min_max_scaling = [time, age, geo_LAT, geo_LON, date.day, date.month]
+    numeric_scaled = min_max_scaler.transform([numeric_to_min_max_scaling])
+    prediction_input = np.concatenate((numeric_scaled, categorical_encoded), axis=1)
+
+    xgb_prediction = xgb_model.predict_proba(prediction_input)
+    sorted_preds = np.sort(xgb_prediction[0])[::-1][:3]
+    top_preds = np.argsort(xgb_prediction[0])[-3:][::-1]
+
+    predicted_class0 = label_encoder.inverse_transform([top_preds[0]])
+    predicted_class1 = label_encoder.inverse_transform([top_preds[1]])
+    predicted_class2 = label_encoder.inverse_transform([top_preds[2]])
+
+    rounded1 = "{:.2f}".format(sorted_preds[0])
+    rounded2 = "{:.2f}".format(sorted_preds[1])
+    rounded3 = "{:.2f}".format(sorted_preds[2])
+
+    text =f"""If you become a victim of a crime, it will be: <ul style='list-style:none'>
+	<li style='font-size:20px'><b><span style='color: red'>{predicted_class0[0]}</span></b> with a probability of <b><span style='color: red;'>{rounded1}</sapn></b>% </li>
+	<li style='font-size:20px'><b><span style='color: red'>{predicted_class1[0]}</span></b> with a probability of <b><span style='color: red'>{rounded2}</span></b>% </li>
+	<li style='font-size:20px'><b><span style='color: red'>{predicted_class2[0]}</span></b> with a probability of <b><span style='color: red'>{rounded3}</span></b>% </li>
+    <ul>"""
+    return text
+
+
+
